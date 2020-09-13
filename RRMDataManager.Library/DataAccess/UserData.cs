a860c517d2f0a052e7d25322d98e1445f422edce
Robert <robertcurry0216@gmail.com>
2020-09-13 14:31:54 +1000

    DataManagerLibrary added

---
+++ RRMDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,23 @@
+﻿using RRMDataManager.Library.Internal.DataAccess;
+using RRMDataManager.Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRMDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<Models.UserModel> GetUserById(string id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+            var p = new { Id = id };
+
+            var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "RRMData");
+
+            return output;
+        }
+    }
+}
